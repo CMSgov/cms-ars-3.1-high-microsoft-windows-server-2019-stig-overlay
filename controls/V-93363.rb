3fac48a5b3ec2f2e9a129ee3d324bc22be7b039e
Shivani Karikar <karikarshivani@gmail.com>
2020-07-27 20:45:02 -0400

    Initial push

---
+++ controls/V-93363.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+
+include_controls 'microsoft-windows-server-2019-stig-baseline' do
+  control "V-93363" do
+    desc  "fix", "Ensure the following mitigations are turned \"ON\" for WINWORD.EXE:
+
+      DEP:
+      Enable: ON
+
+      ASLR:
+      ForceRelocateImages: ON
+
+      Payload:
+      EnableExportAddressFilter: ON
+      EnableExportAddressFilterPlus: ON
+      EnableImportAddressFilter: ON
+      EnableRopStackPivot: ON
+      EnableRopCallerCheck: ON
+      EnableRopSimExec: ON"
+  end
+end
