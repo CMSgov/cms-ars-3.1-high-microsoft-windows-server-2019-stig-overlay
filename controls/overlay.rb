62ea047e76da422a5edaa27094601b69372adda3
karikarshivani <karikarshivani@gmail.com>
2020-07-29 13:24:44 -0400

    Completed the (Not in ARS) and Skipped controls
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,6 +1,78 @@
 # encoding: UTF-8
 
-include_controls 'microsoft-windows-server-2019-stig-baseline' do
+include_controls "microsoft-windows-server-2019-stig-baseline" do
+
+  control "V-93019" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93021" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93023" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93183" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93185" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93187" do
+    title "The Windows Server 2019 time service must synchronize with an appropriate CMS time source."
+    desc  'check', "Review the Windows time service configuration.
+    Open an elevated \"Command Prompt\" (run as administrator).
+    Enter \"W32tm /query /configuration\".
+    Domain-joined systems (excluding the domain controller with the PDC emulator role):
+    If the value for \"Type\" under \"NTP Client\" is not \"NT5DS\", this is a finding.
+
+    Other systems:
+    If systems are configured with a \"Type\" of \"NTP\", including standalone systems and the domain controller with the PDC Emulator role, and do not have a CMS time server defined for \"NTPServer\", this is a finding.
+
+    CMS-approved time servers include:
+    - NIST Internet Time Servers (http://tf.nist.gov/tf-cgi/servers.cgi)
+    - U.S. Naval Observatory Stratum-1 NTP Servers (http://tycho.usno.navy.mil/ntp.html); and
+    - CMS designated internal NTP time servers providing an NTP Stratum-2 service to the above servers
+
+    To determine the domain controller with the PDC Emulator role:
+    Open \"PowerShell\".
+    Enter \"Get-ADDomain | FT PDCEmulator\"."
+    desc  'fix', "Configure the system to synchronize time with an appropriate CMS time source.
+    Domain-joined systems use NT5DS to synchronize time from other systems in the domain by default.
+    If the system needs to be configured to an NTP server, configure the system to point to an authorized time server by setting the policy value for Computer Configuration >> Administrative Templates >> System >> Windows Time Service >> Time Providers >> \"Configure Windows NTP Client\" to \"Enabled\", and configure the \"NtpServer\" field to point to an appropriate CMS time server.
+
+    CMS-approved time servers include:
+    - NIST Internet Time Servers (http://tf.nist.gov/tf-cgi/servers.cgi)
+    - U.S. Naval Observatory Stratum-1 NTP Servers (http://tycho.usno.navy.mil/ntp.html); and
+    - CMS designated internal NTP time servers providing an NTP Stratum-2 service to the above servers
+
+    Time synchronization will occur through a hierarchy of time servers down to the local level. Clients and lower-level servers will synchronize with an authorized time server in the hierarchy."
+  end
+
+  control "V-93199" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93201" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93203" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
   control "V-93313" do
     desc  "fix", "Ensure Exploit Protection system-level mitigation, \"Data Execution Prevention (DEP)\", is turned on.  The default configuration in Exploit Protection is \"On by default\" which meets this requirement.
 
@@ -446,6 +518,118 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
     EnableRopStackPivot: ON
     EnableRopCallerCheck: ON
     EnableRopSimExec: ON"
+  end
+
+  control "V-93425" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93427" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93429" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93431" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93433" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93435" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93453" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93455" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
+  end
+
+  control "V-93483" do
+    title "Windows Server 2019 domain Controller PKI certificates must be issued by the CMS PKI or an approved External Certificate Authority (ECA)."
+    desc  "check", "This applies to domain controllers. It is NA for other systems.
+      Run \"MMC\".
+      Select \"Add/Remove Snap-in\" from the \"File\" menu.
+      Select \"Certificates\" in the left pane and click the \"Add >\" button.
+      Select \"Computer Account\" and click \"Next\".
+      Select the appropriate option for \"Select the computer you want this snap-in to manage\" and click \"Finish\".
+      Click \"OK\".
+      Select and expand the Certificates (Local Computer) entry in the left pane.
+      Select and expand the Personal entry in the left pane.
+      Select the Certificates entry in the left pane. In the right pane, examine the \"Issued By\" field for the certificate to determine the issuing CA.
+      If the \"Issued By\" field of the PKI certificate being used by the domain controller does not indicate the issuing CA is part of the CMS PKI or an approved ECA, this is a finding.
+      If the certificates in use are issued by a CA authorized by the Component's CIO, this is a CAT II finding."
+    desc  "fix", "Obtain a server certificate for the domain controller issued by the CMS PKI or an approved ECA."
+
+    describe "For this CMS ARS 3.1 overlay, this control must be reviewed manually" do 
+      skip "For this CMS ARS 3.1 overlay, this control must be reviewed manually"
+    end
+  end
+
+  control "V-93485" do
+    title "Windows Server 2019 PKI certificates associated with user accounts must be issued by a CMS PKI or an approved External Certificate Authority (ECA)."
+
+    describe "For this CMS ARS 3.1 overlay, this control must be reviewed manually" do 
+      skip "For this CMS ARS 3.1 overlay, this control must be reviewed manually"
+    end
+  end
+
+  control "V-93487" do
+    title "Windows Server 2019 must have the CMS Root Certificate Authority (CA) certificates installed in the Trusted Root Store."
+    desc  "To ensure secure CMS websites and CMS-signed code are properly validated, the system must trust the CMS Root CAs. The CMS root certificates will ensure that the trust chain is established for server certificates issued from the CMS CAs."
+    desc  "check", "Verify the CMS Root CA certificates are installed in the Trusted Root Store."
+    desc  "fix", "Install the CMS Root CA certificates."
+  
+    describe "For this CMS ARS 3.1 overlay, this control must be reviewed manually" do 
+      skip "For this CMS ARS 3.1 overlay, this control must be reviewed manually"
+    end
+  end
+  
+  control "V-93489" do
+    title "Windows Server 2019 must have the CMS Interoperability Root Certificate Authority (CA) cross-certificates installed in the Untrusted Certificates Store on unclassified systems."
+    desc  "To ensure users do not experience denial of service when performing certificate-based authentication to CMS websites due to the system chaining to a root other than CMS Root CAs, the CMS Interoperability Root CA cross-certificates must be installed in the Untrusted Certificate Store. This requirement only applies to unclassified systems."
+    desc  "check", "This is applicable to unclassified systems. It is NA for others.
+    Verify that the CMS Interoperability Root Certificate Authority (CA) cross-certificates is installed in the Untrusted Certificates Store on unclassified systems."
+    desc  "fix", "Install the CMS Interoperability Root CA cross-certificates on unclassified systems."
+    
+    describe "For this CMS ARS 3.1 overlay, this control must be reviewed manually" do 
+      skip "For this CMS ARS 3.1 overlay, this control must be reviewed manually"
+    end
+  end
+
+  control "V-93491" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the CCEB is not applicable to CMS"
+  end
+
+  control "V-93499" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not mandatory in CMS ARS 3.1"
+  end
+
+  control "V-93501" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not mandatory in CMS ARS 3.1"
+  end
+
+  control "V-93543" do
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
   end
 
 end
--- temp/V-93201.rb
+++ temp/V-93201.rb
@@ -1,33 +1,8 @@
 # encoding: UTF-8
-
+#
 include_controls 'microsoft-windows-server-2019-stig-baseline' do
   control "V-93201" do
-    title "Windows Server 2019 must disable the Windows Installer Always install with elevated privileges option."
-    desc  "Standard user accounts must not be granted elevated privileges. Enabling Windows Installer to elevate privileges when installing applications can allow malicious persons and applications to gain full control of a system."
-    desc  "rationale", ""
-    desc  'check', "If the following registry value does not exist or is not configured as specified, this is a finding:
-
-      Registry Hive: HKEY_LOCAL_MACHINE
-      Registry Path: \\SOFTWARE\\Policies\\Microsoft\\Windows\\Installer\\
-
-      Value Name: AlwaysInstallElevated
-
-      Type: REG_DWORD
-      Value: 0x00000000 (0)"
-    desc  'fix', "Configure the policy value for Computer Configuration >> Administrative Templates >> Windows Components >> Windows Installer >> \"Always install with elevated privileges\" to \"Disabled\"."
-    impact 0.7
-    tag 'severity': nil
-    tag 'gtitle': 'SRG-OS-000362-GPOS-00149'
-    tag 'gid': 'V-93201'
-    tag 'rid': 'SV-103289r1_rule'
-    tag 'stig_id': 'WN19-CC-000430'
-    tag 'fix_id': 'F-99447r1_fix'
-    tag 'cci': ["CCI-001812"]
-    tag 'nist': ["CM-11 (2)", "Rev_4"]
-
-    describe registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Installer') do
-      it { should have_property 'AlwaysInstallElevated' }
-      its('AlwaysInstallElevated') { should cmp 0 }
-    end
+    impact 0.0
+    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
   end
 end
