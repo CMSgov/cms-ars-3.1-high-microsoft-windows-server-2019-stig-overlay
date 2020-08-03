e0e87a1861da6f4400492723b223b370610ae5f5
karikarshivani <karikarshivani@gmail.com>
2020-08-03 10:36:03 -0400

    Input test complete
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- temp/V-93143.rb
+++ temp/V-93143.rb
@@ -25,7 +25,6 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
     tag 'nist': ["AC-7 a", "AC-7 b", "Rev_4"]
 
     os_type = command('Test-Path "$env:windir\explorer.exe"').stdout.strip
-    #password_lock = input('pass_lock_time')
 
     if os_type == 'false'
       describe 'This system is a Server Core Installation, and a manual check will need to be performed with command Secedit /Export /Areas User_Rights /cfg c:\\path\\filename.txt' do
@@ -33,7 +32,6 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
       end
     else
       describe security_policy do
-        #its('ResetLockoutCount') { should be >= password_lock }
         its('ResetLockoutCount') { should be >= input('pass_lock_time') }
       end
     end
