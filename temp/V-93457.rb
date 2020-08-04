c650055588edd9b414a95525796a0787e4fb80fb
karikarshivani <karikarshivani@gmail.com>
2020-08-03 22:29:41 -0400

    Added input unused_account_age and modified it for CMS
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- temp/V-93457.rb
+++ temp/V-93457.rb
@@ -2,14 +2,11 @@
 
 include_controls 'microsoft-windows-server-2019-stig-baseline' do
   control "V-93457" do
-    title "Windows Server 2019 outdated or unused accounts must be removed or disabled."
-    desc  "Outdated or unused accounts provide penetration points that may go undetected. Inactive accounts must be deleted if no longer necessary or, if still required, disabled until needed."
-    desc  "rationale", ""
     desc  "check", "Open \"Windows PowerShell\".
 
       Domain Controllers:
-      Enter \"Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00:00:00\"
-      This will return accounts that have not been logged on to for 35 days, along with various attributes such as the Enabled status and LastLogonDate.
+      Enter \"Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 30.00:00:00\"
+      This will return accounts that have not been logged on to for 30 days, along with various attributes such as the Enabled status and LastLogonDate.
 
       Member servers and standalone systems:
       Copy or enter the lines below to the PowerShell window and enter. (Entering twice may be required. Do not include the quotes at the beginning and end of the query.)
@@ -31,53 +28,9 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
       - Built-in guest account (Renamed, Disabled, SID ending in 501)
       - Application accounts
 
-      If any enabled accounts have not been logged on to within the past 35 days, this is a finding.
+      If any enabled accounts have not been logged on to within the past 30 days, this is a finding.
 
       Inactive accounts that have been reviewed and deemed to be required must be documented with the ISSO."
-    desc  "fix", "Regularly review accounts to determine if they are still active. Remove or disable accounts that have not been used in the last 35 days."
-    impact 0.5
-    tag severity: nil
-    tag gtitle: "SRG-OS-000118-GPOS-00060"
-    tag gid: "V-93457"
-    tag rid: "SV-103543r1_rule"
-    tag stig_id: "WN19-00-000190"
-    tag fix_id: "F-99701r1_fix"
-    tag cci: ["CCI-000795"]
-    tag nist: ["IA-4 e", "Rev_4"]
-
-    application_accounts = input('application_accounts_domain')
-    excluded_accounts = input('excluded_accounts_domain')
-    domain_role = command('wmic computersystem get domainrole | Findstr /v DomainRole').stdout.strip
-    untracked_accounts = []
-
-    if domain_role == '4' || domain_role == '5'
-      ad_accounts = json({ command: 'Search-ADAccount -AccountInactive -UsersOnly -Timespan 35.00:00:00 | Where -Property Enabled -eq $True | Select -ExpandProperty Name | ConvertTo-Json' }).params
-      
-      unless ad_accounts.empty?
-        case ad_accounts
-        when String
-          ( ad_account = [] ) << ad_accounts
-          untracked_accounts = ad_accounts - application_accounts - excluded_accounts
-        when Array
-          untracked_accounts = ad_accounts - application_accounts - excluded_accounts
-        end
-      end
-
-      describe 'AD Accounts' do
-        it 'AD should not have any Accounts that are Inactive over 35 days' do
-          failure_message = "Users that have not logged into in 35 days #{untracked_accounts}"
-          expect(untracked_accounts).to be_empty, failure_message
-        end
-      end
-    else
-      local_accounts = json({ command: "Get-LocalUser | Where-Object {$_.Enabled -eq 'True' -and $_.Lastlogon -le (Get-Date).AddDays(-35) } | Select -ExpandProperty Name | ConvertTo-Json" }).params
-
-      describe "Inactive account or accounts exists" do
-        it 'Server should not have inactive accounts' do
-          failure_message = "User or Users have not logged in to system in 35 days: #{local_accounts}" 
-          expect(local_accounts).to be_empty, failure_message
-        end
-      end
-    end
+    desc  "fix", "Regularly review accounts to determine if they are still active. Remove or disable accounts that have not been used in the last 30 days."
   end
 end
