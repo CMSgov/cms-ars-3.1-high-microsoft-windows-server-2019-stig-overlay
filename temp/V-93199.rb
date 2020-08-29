1c0fd6da9373bf43ad7bd14c6d82567ae10a7add
karikarshivani <karikarshivani@gmail.com>
2020-08-29 09:00:58 -0400

    Removed temporary folder
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- temp/V-93199.rb
+++
@@ -1,8 +0,0 @@
-# encoding: UTF-8
-#
-include_controls 'microsoft-windows-server-2019-stig-baseline' do
-  control "V-93199" do
-    impact 0.5
-    desc, "caveat", "Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1"
-  end
-end
